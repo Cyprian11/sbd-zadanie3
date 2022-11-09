cbe7793b282818d21adebf53303d834e63ea551f
Cyprian11 <barabaszcyprianti@gmail.com>
2022-11-09 16:03:12 +0100

    zadanie 3

--- zapytania.sql
+++ zapytania.sql
@@ -12,7 +12,15 @@ SELECT MIN(price) FROM products GROUP BY category_id;
 SELECT MAX(price) FROM products GROUP BY category_id;
 SELECT SUM(products_id) FROM products;
 SELECT SUM(customers_id) FROM customers;
-SELECT COUNT(customers_id) FROM customers ORDER BY country;
-SELECT COUNT(category_id) AND AVG(price) FROM products   WHERE category_id="1";
+SELECT COUNT(customers_id) FROM customers GROUP BY country;
+SELECT COUNT(category_id) AND AVG(price) FROM products WHERE category_id="1";
 SELECT SUM(id_products) WHERE category_id="2";
 SELECT MAX(price) WHERE category_id="1" AND category_id="2";
+
+
+SELECT * FROM products WHERE price BETWEEN 10 AND 20;
+SELECT * FROM products WHERE price NOT BETWEEN 10 AND 20;
+SELECT * FROM products WHERE category_id IN (1,2,3);
+
+
+
